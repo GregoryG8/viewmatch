caa36e3f83da2280a67d204d6bd98c93619c7fc4
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-05-02 02:23:19 -0500

    First commit

---
+++ src/App.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // System.out.println("Pelicula Matrix");
+
+        // Declaracion de variables
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlanBasico = true;
+        String nombre = "Matrix";
+        String sinopsis = """
+                La mejor pelicula del fin del milenio
+                """;
+
+        double mediaEvaluacionUsuario = 0 ;
+
+        System.out.println("Pelicula: " + nombre);
+        System.out.println(fechaDeLanzamiento);
+        System.out.println(incluidoEnElPlanBasico);
+
+        if (fechaDeLanzamiento >= 2023) {
+            System.out.println("Pelicula Popular en el momento");
+        }else{
+            System.out.println("Pelicula Retro");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("Que nota le darias a Matrix?");
+            double notaMatrix = teclado.nextDouble();
+            mediaEvaluacionUsuario = mediaEvaluacionUsuario + notaMatrix;
+        }
+
+        System.out.println("La media es: " + mediaEvaluacionUsuario / 3);
+
+        //gracias
+    }
+}
