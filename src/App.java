81dc4860fe0b76547f73d586270b3002ad843a6c
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-05-04 13:32:31 -0500

    avance

--- src/App.java
+++ src/App.java
@@ -1,39 +1,47 @@
-import java.util.Scanner;
+import modelo.Movie;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        // System.out.println("Pelicula Matrix");
 
-        // Declaracion de variables
-        int fechaDeLanzamiento = 1999;
-        boolean incluidoEnElPlanBasico = true;
-        String nombre = "Matrix";
-        String sinopsis = """
-                La mejor pelicula del fin del milenio
-                """;
+        Movie matrix = new Movie();
+        matrix.setNombre("Matrix");
+        matrix.setDuracion(120);
+        matrix.setFechaDeLanzamiento(1999);;
 
-        double mediaEvaluacionUsuario = 0 ;
+        matrix.mostrarFicha();
+        
+            // System.out.println("Hello, World!");
+            // // System.out.println("Pelicula Matrix");
 
-        System.out.println("Pelicula: " + nombre);
-        System.out.println(fechaDeLanzamiento);
-        System.out.println(incluidoEnElPlanBasico);
+            // // Declaracion de variables
+            // int fechaDeLanzamiento = 1999;
+            // boolean incluidoEnElPlanBasico = true;
+            // String nombre = "Matrix";
+            // String sinopsis = """
+            //         La mejor pelicula del fin del milenio
+            //         """;
 
-        if (fechaDeLanzamiento >= 2023) {
-            System.out.println("Pelicula Popular en el momento");
-        }else{
-            System.out.println("Pelicula Retro");
-        }
+            // double mediaEvaluacionUsuario = 0 ;
 
-        for (int i = 0; i < 3; i++) {
-            Scanner teclado = new Scanner(System.in);
-            System.out.println("Que nota le darias a Matrix?");
-            double notaMatrix = teclado.nextDouble();
-            mediaEvaluacionUsuario = mediaEvaluacionUsuario + notaMatrix;
-        }
+            // System.out.println("Pelicula: " + nombre);
+            // System.out.println(fechaDeLanzamiento);
+            // System.out.println(incluidoEnElPlanBasico);
 
-        System.out.println("La media es: " + mediaEvaluacionUsuario / 3);
+            // if (fechaDeLanzamiento >= 2023) {
+            //     System.out.println("Pelicula Popular en el momento");
+            // }else{
+            //     System.out.println("Pelicula Retro");
+            // }
 
-        //gracias
+            // for (int i = 0; i < 3; i++) {
+            //     Scanner teclado = new Scanner(System.in);
+            //     System.out.println("Que nota le darias a Matrix?");
+            //     double notaMatrix = teclado.nextDouble();
+            //     mediaEvaluacionUsuario = mediaEvaluacionUsuario + notaMatrix;
+            // }
+
+            // System.out.println("La media es: " + mediaEvaluacionUsuario / 3);
+
+            // //gracias
     }
 }
